aa994e32ff7fd34138acae6c694d7fb2ef457595
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-01-20 19:46:19 +0300

    Initial commit

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -7,8 +7,11 @@ from .utils import api_get_or_404
 
 @app.route('/api/id/', methods=('POST',))
 def api_create():
-    url_map = URLMap().create(db, request.get_json())
-    return jsonify(url_map.to_representation()), 201
+    # url_map = URLMap().create(db, request.get_json())
+    return jsonify(
+        # url_map.to_representation()
+        URLMap().create(db, request.get_json()).to_representation()
+    ), 201
 
 
 @app.route('/api/id/<short_id>/')
