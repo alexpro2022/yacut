0f76677e10e1a410ace51026fc42482d78ebef49
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-01-22 00:16:23 +0300

    5

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,18 +1,19 @@
 from http import HTTPStatus
+from typing import Literal, Tuple
 
-from flask import jsonify, request
+from flask import jsonify, request, Response
 
 from yacut import app, db
 from yacut.models import URLMap
 
 
 @app.route('/api/id/', methods=('POST',))
-def api_create():
+def api_create() -> Tuple[Response, Literal[HTTPStatus.CREATED]]:
     return jsonify(
         URLMap().create(db, request.get_json()).to_representation()
     ), HTTPStatus.CREATED
 
 
 @app.route('/api/id/<short_id>/')
-def api_get_link(short_id):
+def api_get_link(short_id) -> Tuple[Response, Literal[HTTPStatus.OK]]:
     return jsonify(url=URLMap().get_original_link(short_id)), HTTPStatus.OK
--- yacut/utils.py
+++ yacut/utils.py
@@ -1,4 +1,5 @@
 import random
+from typing import Any
 
 from werkzeug.exceptions import NotFound
 
@@ -10,14 +11,14 @@ def get_short_id() -> str:
     return ''.join(random.choice(ALPHABET) for i in range(CUSTOM_ID_SIZE_AUTO))
 
 
-def get_unique_id(model, field) -> str:
+def get_unique_id(model: Any, field: Any) -> str:
     unique_id = get_short_id()
     while model.query.filter(field == unique_id).count():
         unique_id = get_short_id()
     return unique_id
 
 
-def get_or_404(model, field, criterion, api=True):
+def get_or_404(model: Any, field: Any, criterion: Any, api=True) -> Any:
     try:
         obj = model.query.filter(field == criterion).first_or_404()
     except NotFound:
