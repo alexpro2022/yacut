b6b25c8a86d0dfe2851e1688405a11c6a6d8a9e2
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-01-22 01:32:26 +0300

    Everithing but the hints

--- yacut/utils.py
+++ yacut/utils.py
@@ -1,9 +1,6 @@
 import random
 
-# from werkzeug.exceptions import NotFound
-
 from settings import ALPHABET, CUSTOM_ID_SIZE_AUTO
-# from yacut.exceptions import InvalidAPIUsage
 
 
 def get_short_id() -> str:
@@ -15,15 +12,3 @@ def get_unique_id(model, field) -> str:
     while model.query.filter(field == unique_id).count():
         unique_id = get_short_id()
     return unique_id
-
-
-'''
-def get_or_404(model, field, criterion, api=True):
-    try:
-        obj = model.query.filter(field == criterion).first_or_404()
-    except NotFound:
-        if api:
-            raise InvalidAPIUsage('Указанный id не найден', 404)
-        raise NotFound
-    return obj
-'''
