a53dc3684d7bb79f7cd4f51b6dffae67e4fe6abd
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-01-22 01:39:58 +0300

    Addee hints

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import Tuple
 
 from flask import render_template, jsonify
 
@@ -7,16 +8,16 @@ from yacut.exceptions import InvalidAPIUsage
 
 
 @app.errorhandler(HTTPStatus.NOT_FOUND)
-def page_not_found(error):
+def page_not_found(error) -> Tuple[str, int]:
     return render_template('404.html'), HTTPStatus.NOT_FOUND
 
 
 @app.errorhandler(HTTPStatus.INTERNAL_SERVER_ERROR)
-def internal_error(error):
+def internal_error(error) -> Tuple[str, int]:
     db.session.rollback()
     return render_template('500.html'), HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 @app.errorhandler(InvalidAPIUsage)
-def invalid_api_usage(error):
+def invalid_api_usage(error) -> Tuple[str, int]:
     return jsonify(error.to_representation()), error.status_code
