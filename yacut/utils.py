f8b90fadc8e196757bcc163ec85376995e670eb5
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-01-22 01:58:09 +0300

    Added Any

--- yacut/utils.py
+++ yacut/utils.py
@@ -1,4 +1,5 @@
 import random
+from typing import Any
 
 from settings import ALPHABET, CUSTOM_ID_SIZE_AUTO
 
@@ -7,7 +8,7 @@ def get_short_id() -> str:
     return ''.join(random.choice(ALPHABET) for i in range(CUSTOM_ID_SIZE_AUTO))
 
 
-def get_unique_id(model, field) -> str:
+def get_unique_id(model: Any, field: Any) -> str:
     unique_id = get_short_id()
     while model.query.filter(field == unique_id).count():
         unique_id = get_short_id()
